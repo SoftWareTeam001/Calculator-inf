73d687b8b148dfdcccefa871086a7b5cd9d77b4d
Maker <maker@qq.com>
2018-08-24 13:23:22 +0800

    完成界面

---
+++ app/src/main/java/com/example/test/calculatorinf/MyButton.java
@@ -0,0 +1,11 @@
+package com.example.test.calculatorinf;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatButton;
+import android.widget.Button;
+
+public class MyButton extends AppCompatButton{
+    public MyButton(Context context){
+        super(context);
+    }
+}
