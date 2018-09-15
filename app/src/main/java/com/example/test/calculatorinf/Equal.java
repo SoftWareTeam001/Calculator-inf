62941499919b7d8c7f7e027902ccaaffc298538c
Maker <maker@qq.com>
2018-09-15 21:17:55 +0800

    完善第一页面，增加清除、左移、排列组合等功能

--- app/src/main/java/com/example/test/calculatorinf/Equal.java
+++ app/src/main/java/com/example/test/calculatorinf/Equal.java
@@ -24,28 +24,28 @@ public class Equal implements View.OnClickListener {
     }
     @Override
     public void onClick(View v){
-        Log.i(TAG,handleFormulaString());
+        Log.i(TAG,"handleResult"+handleFormulaString());
         webView.post(new Runnable() {
              @Override
              public void run() {
-                 webView.evaluateJavascript("javascript:calculate("+handleFormulaString()+")", new ValueCallback<String>() {
-                     @Override
-                     public void onReceiveValue(String value) {
-                         MyString.ResultString=value;
-                         textView.setText(MyString.ResultString);
-                         Log.i(TAG,MyString.ResultString);
-                         if(!value.equals("null")){
-                             try{
-                                 MyLog myLog=new MyLog(mContext);
-                                 myLog.WriteLog();
-                                 reset();
-                             }
-                             catch (Exception e){
-                                 e.printStackTrace();
-                             }
+             webView.evaluateJavascript("javascript:calculate("+handleFormulaString()+")", new ValueCallback<String>() {
+                 @Override
+                 public void onReceiveValue(String value) {
+                     MyString.ResultString=value;
+                     textView.setText(MyString.ResultString);
+                     Log.i(TAG,MyString.ResultString);
+                     if(value=="null"){
+                         try{
+                             MyLog myLog=new MyLog(mContext);
+                             myLog.WriteLog();
+                             reset();
+                         }
+                         catch (Exception e){
+                             e.printStackTrace();
                          }
                      }
-                 });
+                 }
+             });
              }
          });
 }
@@ -58,32 +58,60 @@ public class Equal implements View.OnClickListener {
         initString=initString.replaceFirst(Regex, "");
         Regex="(\\}\\$\\$)";
         initString=initString.replaceFirst(Regex, "");
-
+        Log.i(TAG,initString);
+        //解决组合
+        initString=initString.replaceAll("C_\\{","math.combinations(");
+        initString=initString.replaceAll("(?<=math.combinations\\(.{1,100})\\}\\^\\{",",");
+        initString=initString.replaceAll("(?<=math.combinations\\(.{1,100},.{1,100})\\}",")");
+        //解决排列
+        initString=initString.replaceAll("P_\\{","math.permutations(");
+        initString=initString.replaceAll("(?<=math.permutations\\(.{1,100})\\}\\^\\{",",");
+        initString=initString.replaceAll("(?<=math.permutations\\(.{1,100},.{1,100})\\}",")");
+        //解决根号
+        initString=initString.replaceAll("\\\\sqrt\\[2\\]\\{","math.sqrt(");
+        initString=initString.replaceAll("(?<=math.sqrt\\(.{1,100})\\}",")");
         //解决power
-        Regex="(\\d+?^)";
-        initString=initString.replaceAll("(?=\\d+\\^)","Math.pow(");
-        initString=initString.replaceAll("(?<=\\^\\{\\d{1,100})\\}", ")");
+        initString=initString.replaceAll("(?=.+\\^)","math.pow(");
+        initString=initString.replaceAll("(?<=\\^\\{.{1,100})\\}", ")");
         initString=initString.replaceAll("\\^\\{", ",");
         //解决三角函数
-        initString=initString.replaceAll("\\\\sin","Math.sin");
-        initString=initString.replaceAll("\\\\cos","Math.cos");
-        initString=initString.replaceAll("\\\\tan","Math.cos");
-        initString=initString.replaceAll("\\\\arcsin","Math.asin");
-        initString=initString.replaceAll("\\\\arccos","Math.acos");
-        initString=initString.replaceAll("\\\\arctan","Math.atan");
+        if(ControlVar.rad){
+            initString=initString.replaceAll("\\\\sin","math.sin");
+            initString=initString.replaceAll("\\\\cos","math.cos");
+            initString=initString.replaceAll("\\\\tan","math.cos");
+            initString=initString.replaceAll("\\\\arcsin","math.asin");
+            initString=initString.replaceAll("\\\\arccos","math.acos");
+            initString=initString.replaceAll("\\\\arctan","math.atan");
+        }
         //解决符号
         initString=initString.replaceAll("\\\\times","*");
         initString=initString.replaceAll("\\\\div","/");
         //解决对数
-        initString=initString.replaceAll("\\\\ln","Math.log");
+        initString=initString.replaceAll("\\\\ln","math.log");
         initString=initString.replaceAll("\\\\lg","lg");
         //解决pi
-        initString=initString.replaceAll("\\\\pi","Math.PI");
+        initString=initString.replaceAll("\\\\pi","math.PI");
+        //res
+        MyLog myLog=new MyLog(MainActivity.getMainActivity());
+        String res=myLog.GetLastLog()[1];
+        Log.i(TAG,"res:"+res);
+        if(res!=""){
+            initString=initString.replaceAll("Res",res);
+            MyLog.currentLog-=1;
+        }
+        else{
+            initString=initString.replaceAll("Res","0");
+        }
+        //删除删除时保留的括号
+        initString=initString.replaceAll("\\{","").replaceAll("\\}","");
         return initString;
     }
     public void reset(){
-        MyString.FormulaString="$$ \\Huge \\textcolor{cyan} {}$$";
-        FormulaView.leftMove=0;
+        MyString.FormulaString=MyString.InitString;
+        MyString.ResultString="";
+        ControlVar.leftMove=0;
+        ControlVar.sub=false;
+        ControlVar.Shift=false;
         MyLog.currentLog=0;
     }
 }
